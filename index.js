6822ab33f28e1be14fb7371f5ad02142f19485e3
O'Brian Saavedra <78567786+obrian-code@users.noreply.github.com>
2022-05-19 21:50:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { app } from "./server/app.js";
 
-const PORT = process.env.PORT || 3050;
-
-app.listen(PORT, () => console.log(`The server has been initialized: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`The server has been initialized: http://localhost:${3000}`))
